5672759a16dfacbc24531dcebd762e29d1279cda
memoryleak47 <memoryleak47@protonmail.ch>
2017-09-06 14:03:03 +0200

    ...

--- src/packet.rs
+++ src/packet.rs
@@ -33,11 +33,11 @@ fn test_packet_serialization() {
 	let b = p1.serialize().unwrap();
 	let p2 = Packet::deserialize(&b).unwrap();
 	match p2 {
-		Packet::File{name: n, content: c} => {
+		Packet::File { name: n, content: c } => {
 			assert_eq!(n, String::from("NAME"));
 			assert_eq!(c, String::from("CONTENT"));
 		},
-		Packet::Directory{name: n, ..} => panic!("p2 should be File"),
+		Packet::Directory { .. } => panic!("p2 should be File"),
 	}
 }
 
@@ -55,16 +55,16 @@ fn test_packet_serialization_dir() {
 	let dir2 = Packet::deserialize(&b).unwrap();
 
 	match dir2 {
-		Packet::File{..} => panic!("dir2 should be a Directory"),
+		Packet::File { .. }  => panic!("dir2 should be a Directory"),
 		Packet::Directory{name: n, packets: p} => {
 			assert_eq!(String::from("thisDir"), n);
 			assert_eq!(1, p.len());
 			match &p[0] {
-				&Packet::File{name: ref n, content: ref c} => {
+				&Packet::File { name: ref n, content: ref c } => {
 					assert_eq!(n, "NAME");
 					assert_eq!(c, "CONTENT");
 				},
-				&Packet::Directory{name: ref n, ..} => panic!("p should be File"),
+				&Packet::Directory { .. } => panic!("p should be File"),
 			}
 		}
 	}
