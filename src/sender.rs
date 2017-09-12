9da7c1fd7ed5614c1588e3228e0e294b8e526c12
Bluemi <bruno.schilling@protonmail.ch>
2017-09-12 16:14:33 +0200

    sender uses FileAppend now

--- src/sender.rs
+++ src/sender.rs
@@ -13,6 +13,8 @@ use bincode;
 
 use PORT;
 
+const MAX_CONTENT_SIZE : usize = 1024 * 1024;
+
 pub fn call<T: Iterator<Item=String>>(mut args: T) {
 	let ip = args.next()
 		.expect("Ip address missing");
@@ -62,21 +64,38 @@ fn send_dir(stream: &mut TcpStream, path: &PathBuf) -> Result<(), String> {
 }
 
 fn send_file(file: &mut File, stream: &mut TcpStream, path: &PathBuf) -> Result<(), String> {
-	let mut contents : Vec<u8> = Vec::new();
-	match file.read_to_end(&mut contents) {
-		Ok(_) => {
-			let path_str: &str = path.to_str()
-				.ok_or_else(|| String::from("cant transform path into string"))?;
-			let packet: Packet = Packet::FileCreate {
-				path: String::from(path_str),
-				content: contents,
-			};
-			send_packet(&packet, stream).unwrap();
-		},
-		Err(err) => {
-			let path_str: &str = path.to_str()
-				.ok_or_else(|| String::from("cant transform path into string"))?;
-			return Err(format!("couldnt read from \"{}\": {:?}", path_str, err))
+	let mut file_completely_sent : bool = false;
+	let mut file_started : bool = true;
+	while !file_completely_sent {
+		let mut contents : Vec<u8> = vec![0; MAX_CONTENT_SIZE];
+		match file.read(&mut contents) {
+			Ok(send_size) => {
+				if send_size != MAX_CONTENT_SIZE {
+					file_completely_sent = true;
+					contents.truncate(send_size);
+				}
+				let path_str: &str = path.to_str()
+					.ok_or_else(|| String::from("cant transform path into string"))?;
+				if file_started {
+					let packet: Packet = Packet::FileCreate {
+						path: String::from(path_str),
+						content: contents,
+					};
+					send_packet(&packet, stream).unwrap();
+				} else {
+					let packet: Packet = Packet::FileAppend {
+						path: String::from(path_str),
+						content: contents,
+					};
+					send_packet(&packet, stream).unwrap();
+				}
+				file_started = false;
+			},
+			Err(err) => {
+				let path_str: &str = path.to_str()
+					.ok_or_else(|| String::from("cant transform path into string"))?;
+				return Err(format!("couldnt read from \"{}\": {:?}", path_str, err))
+			}
 		}
 	}
 	Ok(())
@@ -85,11 +104,12 @@ fn send_file(file: &mut File, stream: &mut TcpStream, path: &PathBuf) -> Result<
 fn send_packet(packet: &Packet, stream: &mut TcpStream) -> Result<(), String> {
 	let arr = packet.serialize().map_err(|x| x.to_string())?;
 	let len_vec = bincode::serialize(&(arr.len() as u64), bincode::Infinite).map_err(|x| x.to_string())?;
+	println!("size={}", arr.len());
 	if let Err(err) = stream.write(&len_vec) {
-		println!("failed to write in stream! {}", err.to_string());
+		return Err(format!("failed to write len in stream! {}", err.to_string()));
 	}
 	if let Err(err) = stream.write(&arr) {
-		println!("failed to write in stream! {}", err.to_string());
+		return Err(format!("failed to write in stream! {}", err.to_string()));
 	}
 	Ok(())
 }
